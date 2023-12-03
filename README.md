a6101e51b70219d8098608d52849c23740de129a
jose leonel <90006477+joseleonelmacedo@users.noreply.github.com>
2023-12-03 14:49:00 -0300

    02

--- README.md
+++ README.md
@@ -1,2 +1,138 @@
-# backend_baterponto
- 
+# Running the application
+
+### Install Python
+
+### Install virtual env
+```
+pip install virtualenv
+```
+
+### Activate virtualenv
+
+#### Navigate until the root folder
+```
+python -m virtualenv venv
+```
+
+#### Activate virtual env
+```
+. venv/Scripts/activate
+```
+
+#### Install dependencies
+```
+pip install -r requirements.txt
+```
+
+#### Install dependencies
+```
+python src/app.py
+```
+
+
+## Endpoints
+
+### 1. Cadastro de Usuário
+
+- **Endpoint:** `/sidi_ponto/v1/cadastro`
+- **Método:** `POST`
+- **Corpo (JSON):**
+  ```json
+  {
+    "name": "Seu Nome",
+    "email": "seu.email@exemplo.com",
+    "password": "sua_senha"
+  }
+  ```
+
+### 2. Login de Usuário
+
+- **Endpoint:** `/sidi_ponto/v1/login`
+- **Método:** `POST`
+- **Corpo (JSON):**
+  ```json
+  {
+    "email": "seu.email@exemplo.com",
+    "password": "sua_senha"
+  }
+  ```
+
+### 3. Registrar Entrada/Saída de Ponto
+
+- **Endpoint:** `/sidi_ponto/v1/criar`
+- **Método:** `POST`
+- **Corpo (JSON):**
+  ```json
+  {
+    "email": "seu.email@exemplo.com",
+    "date": "DD-MM-AAAA",   // 23/11/2023
+    "entries": "HH:MM",  // 08:00 -> Opcional
+    "exits": "HH:MM"     // 18:00 -> Opcional
+  }
+  ```
+
+### 4. Listar Registros de Ponto do Usuário
+
+- **Endpoint:** `/sidi_ponto/v1/listar`
+- **Método:** `POST`
+- **Corpo (JSON):**
+  ```json
+  {
+    "email": "seu.email@exemplo.com"
+  }
+  ```
+
+## Exemplos de Requisições usando o Postman
+
+1. **Cadastro de Usuário:**
+   - **URL:** `http://localhost:5000/sidi_ponto/v1/cadastro`
+   - **Corpo (JSON):**
+     ```json
+     {
+       "name": "Fulano de Tal",
+       "email": "fulano@exemplo.com",
+       "password": "senha_segura"
+     }
+     ```
+
+2. **Login de Usuário:**
+   - **URL:** `http://localhost:5000/sidi_ponto/v1/login`
+   - **Corpo (JSON):**
+     ```json
+     {
+       "email": "fulano@exemplo.com",
+       "password": "senha_segura"
+     }
+     ```
+
+3. **Registrar Entrada de Ponto:**
+   - **URL:** `http://localhost:5000/sidi_ponto/v1/criar`
+   - **Corpo (JSON):**
+     ```json
+     {
+       "email": "fulano@exemplo.com",
+       "date": "2023-11-23",
+       "entries": "09:00",
+       "exits": ""
+     }
+     ```
+    
+3. **Registrar Saída de Ponto:**
+   - **URL:** `http://localhost:5000/sidi_ponto/v1/criar`
+   - **Corpo (JSON):**
+     ```json
+     {
+       "email": "fulano@exemplo.com",
+       "date": "2023-11-23",
+       "entries": "",
+       "exits": "17:00"
+     }
+
+4. **Listar Registros de Ponto do Usuário:**
+   - **URL:** `http://localhost:5000/sidi_ponto/v1/listar`
+   - **Corpo (JSON):**
+     ```json
+     {
+       "email": "fulano@exemplo.com"
+     }
+     ```
--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 users_database = os.path.join(os.path.dirname(__file__), 'database', 'users.txt')
+registers_database = os.path.join(os.path.dirname(__file__), 'database', 'registers.txt')
 
 def check_user_credentials(email, password):
     # Read the user data from the text file and check if the credentials match
@@ -18,6 +19,21 @@ def check_user_credentials(email, password):
         print("Error on check_user_credentials: ", ex)
     return False
 
+def get_users(email):
+    # Read the user data from the text file and check if the credentials match
+    users = []
+    try:
+        with open(registers_database, 'r', encoding="utf-8") as file:
+            for line in file:
+                user_data = json.loads(line)
+                print("user_data > ", user_data)
+                if user_data.get("email") == email:
+                    users.append(user_data)
+
+    except Exception as ex:
+        print("Error on get_users: ", ex)
+
+    return users
 
 @app.route('/sidi_ponto/v1/cadastro', methods=['POST'])
 def cadastro():
@@ -27,7 +43,7 @@ def cadastro():
     if 'name' in data and 'email' in data and 'password' in data:
         user = {
             'name': data['name'],
-            'email_': data['email'],
+            'email': data['email'],
             'password': data['password']
         }
 
@@ -63,6 +79,47 @@ def login():
         response = jsonify({'message': 'Missing email or password', 'status_code': 400}), 400
         return response
 
+
+@app.route('/sidi_ponto/v1/criar', methods=['POST'])
+def criar_ponto():
+    data = json.loads(request.data)
+    response = None
+
+    if 'email' in data and 'date' in data and ('entries' in data or 'exits' in data):
+        item = {
+            'email': data['email'],
+            'date': data['date'],
+            'entries': data.get('entries', None),
+            'exits': data.get('exits', None) 
+        }
+
+        try:
+            with open(registers_database, 'a', encoding="utf-8") as json_file:
+                json_file.write(json.dumps(item) + '\n')
+            response = jsonify({'message': 'Ponto registradom com sucesso!', 'status_code': 200}), 200
+            return response
+        except Exception as ex:
+            print("Error on cadastro: ", ex)
+
+    response = jsonify({'message': 'Error ao bater o ponto. Verifique as informação e tente novamente.', 'status_code': 400}), 400
+    return response
+
+
+@app.route('/sidi_ponto/v1/listar', methods=['POST'])
+def listar_ponto():
+    data = json.loads(request.data)
+    response = None
+
+    if 'email' in data:
+        try:
+            users = get_users(data["email"])
+            response = jsonify({'data': users, 'status_code': 200}), 200
+            return response
+        except Exception as ex:
+            print("Error on listar_ponto: ", ex)
+
+    response = jsonify({'message': 'Error ao recuperar ponto.', 'status_code': 400}), 400
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
